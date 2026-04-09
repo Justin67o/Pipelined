176ec3a0465cad1b98abda3c98e3c2ea1e8e4a39
Justin <jg3chen@uwaterloo.ca>
2026-04-09 13:02:58 -0400

    add auth

---
+++ pipelined/src/lib/requireAuth.ts
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+import { prisma } from "./prisma";
+
+export async function requireAuthentication(){
+
+    const session = await getServerSession(authOptions);
+    if(!session?.user?.email){
+        throw new Error ("Unauthorized");
+    }
+    const user = await prisma.user.findUnique({
+        where: { email: session.user.email },
+    });
+
+    if (!user){
+        throw new Error ("Unauthorized")
+    }
+    return user;
+}
